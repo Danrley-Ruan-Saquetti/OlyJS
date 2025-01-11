cc45fa0b9b1bbcff722d4a39c6b9f8ba149e5034
Dan Ruan <danrsaquetti@gmail.com>
2025-01-11 20:23:08 -0300

    feat: Creating game object CameraGameObject

---
+++ src/entities/camera.entity.ts
@@ -0,0 +1,5 @@
+import { GameObject } from './game-object.entity.js'
+
+export class CameraGameObject extends GameObject {
+
+}
--- src/entities/index.ts
+++ src/entities/index.ts
@@ -1,1 +1,2 @@
 export * from './game-object.entity.js'
+export * from './camera.entity.js'
