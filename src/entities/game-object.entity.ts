842c775ebb4ef5a55d7d72287f607e27e2b95199
Dan Ruan <danrsaquetti@gmail.com>
2025-01-11 16:24:31 -0300

    feat: Adding IContainerComponent interface implementation to GameObject class

--- src/entities/game-object.entity.ts
+++ src/entities/game-object.entity.ts
@@ -1,10 +1,12 @@
 import { CanvasRenderer, DeltaTime } from '../utils/index.js'
-import { IGameObject } from '../interfaces/index.js'
-import { TransformComponent } from '../components/transform.component.js'
+import { IContainerComponent, IGameObject } from '../interfaces/index.js'
+import { GameComponent, TransformComponent } from '../components/index.js'
+import { ContainerComponentRepository } from '../repositories/index.js'
 
-export class GameObject implements IGameObject {
+export class GameObject implements IGameObject, IContainerComponent {
 
   private _isDestroyed = false
+  private containerComponent = new ContainerComponentRepository()
 
   readonly transform = new TransformComponent()
 
@@ -24,5 +26,21 @@ export class GameObject implements IGameObject {
 
   isDestroyed() {
     return this._isDestroyed
+  }
+
+  addComponent(...components: GameComponent[]) {
+    return this.containerComponent.addComponent(...components)
+  }
+
+  hasComponent(classComponent: new (...args: any) => GameComponent) {
+    return this.containerComponent.hasComponent(classComponent)
+  }
+
+  getComponent<T extends GameComponent>(classComponent: new (...args: any) => T) {
+    return this.containerComponent.getComponent(classComponent)
+  }
+
+  getComponents() {
+    return this.containerComponent.getComponents()
   }
 }
