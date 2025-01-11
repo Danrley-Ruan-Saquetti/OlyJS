43417457ac08adaaa3c44549bb67101ef55df76a
Dan Ruan <danrsaquetti@gmail.com>
2025-01-11 16:23:08 -0300

    feat: Creating IContainerComponent interface

---
+++ src/interfaces/container-component.interface.ts
@@ -0,0 +1,8 @@
+import { GameComponent } from '../components/index.js'
+
+export interface IContainerComponent {
+  addComponent(...components: GameComponent[]): void
+  hasComponent(classComponent: new (...args: any) => GameComponent): boolean
+  getComponent<T extends GameComponent>(classComponent: new (...args: any) => T): T | null
+  getComponents(): GameComponent[]
+}
