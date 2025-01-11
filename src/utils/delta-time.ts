bb890dd3a9ef0cc3c6219e6876176505b799bf60
Dan Ruan <danrsaquetti@gmail.com>
2025-01-10 21:38:42 -0300

    feat: Implementing the DeltaTime calculation function

--- src/utils/delta-time.ts
+++ src/utils/delta-time.ts
@@ -1,3 +1,28 @@
 export class DeltaTime {
 
+  private _lastFrameTime = 0
+  private _deltaTime = 0
+  private _deltaTimeTotal = 0
+
+  get deltaTimeMilliseconds() { return this._deltaTime }
+  get deltaTimeSeconds() { return this._deltaTime / 1_000 }
+  get deltaTimeTotalMilliseconds() { return this._deltaTimeTotal }
+  get deltaTimeTotalSeconds() { return this._deltaTimeTotal / 1_000 }
+  get FPS() {
+    const deltaTimeSeconds = this.deltaTimeSeconds
+    return deltaTimeSeconds > 0 ? Math.round(1 / deltaTimeSeconds) : 0
+  }
+
+  start() {
+    this._deltaTime = performance.now()
+    this._deltaTimeTotal = 0
+  }
+
+  calculate() {
+    const current = performance.now()
+
+    this._deltaTime = current - this._lastFrameTime
+    this._deltaTimeTotal += this._deltaTime
+    this._lastFrameTime = current
+  }
 }
