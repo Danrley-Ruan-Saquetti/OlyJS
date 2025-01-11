64a48476d967f2bfd189c6aa6642c2f145df792a
Dan Ruan <danrsaquetti@gmail.com>
2025-01-11 00:14:56 -0300

    feat: Implementing Keyboard system

--- src/systems/keyboard-input.system.ts
+++ src/systems/keyboard-input.system.ts
@@ -1,17 +1,28 @@
-export class KeyboardSystem {
+import { Keys } from '../enums/index.js'
+import { Input } from '../states/input.state.js'
+import { GameSystem } from './system.js'
+
+export class KeyboardSystem extends GameSystem {
+
+  constructor() {
+    super()
+  }
 
   start() {
     document.addEventListener('keydown', this.onKeyDown)
-    document.addEventListener('keyup', () => {
-
-    })
+    document.addEventListener('keyup', this.onKeyUp)
   }
 
   stop() {
     document.removeEventListener('keydown', this.onKeyDown)
+    document.removeEventListener('keyup', this.onKeyUp)
   }
 
-  private onKeyDown() {
-    console.log(this)
+  private onKeyDown = (event: KeyboardEvent) => {
+    Input.keyboard.press(Keys[event.code])
+  }
+
+  private onKeyUp = (event: KeyboardEvent) => {
+    Input.keyboard.release(Keys[event.code])
   }
 }
