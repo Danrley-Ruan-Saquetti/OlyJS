a281b275b11f2afaec6425424319186f6fcc029b
Dan Ruan <danrsaquetti@gmail.com>
2025-01-11 19:17:32 -0300

    feat: Creating drawRectangle method in CanvasRenderer

--- src/utils/canvas-renderer.ts
+++ src/utils/canvas-renderer.ts
@@ -1,8 +1,15 @@
-import { ContextRender2D } from '../interfaces/index.js'
+import { ContextRender2D, IRectangle, IVector2 } from '../interfaces/index.js'
 
 export class CanvasRenderer {
 
   constructor(
     public readonly ctx: ContextRender2D
   ) { }
+
+  drawRectangle({ x, y, height, width, color }: IVector2 & IRectangle & { color?: string }) {
+    this.ctx.beginPath()
+    this.ctx.rect(x, y, width, height)
+    if (color) this.ctx.fillStyle = color
+    this.ctx.fill()
+  }
 }
