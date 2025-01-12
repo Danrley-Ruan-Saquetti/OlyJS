5960befa4052dff88892bbbd5151722987316c7d
Dan Ruan <danrsaquetti@gmail.com>
2025-01-12 14:03:17 -0300

    refactor: Using the IDestroyable interface in the IGameObject interface

--- src/interfaces/game-object.interface.ts
+++ src/interfaces/game-object.interface.ts
@@ -1,9 +1,7 @@
+import { IDestroyable } from './destroyable.interface.js'
 import { IMonoBehaviour } from './mono-behaviour.interface.js'
 import { IRenderable } from './renderable.interface.js'
 
-export interface IGameObject extends IMonoBehaviour, IRenderable {
-  destroy(): void
+export interface IGameObject extends IMonoBehaviour, IRenderable, IDestroyable {
   startComponents?(): void
-  onDestroy?(): void
-  isDestroyed(): boolean
 }
