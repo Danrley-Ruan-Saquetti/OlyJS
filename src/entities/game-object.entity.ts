f95eb18f39fabe19ece8433e3fd74f5fe18973dc
Dan Ruan <danrsaquetti@gmail.com>
2025-01-19 15:14:37 -0300

    feat: integrating ContainerTagRepository with GameObject

--- src/entities/game-object.entity.ts
+++ src/entities/game-object.entity.ts
@@ -1,13 +1,14 @@
 import { CanvasRenderer, DeltaTime } from '../utils/index.js'
-import { IGameObject } from '../interfaces/index.js'
+import { IContainerComponent, IGameObject, ITaggable, Tag } from '../interfaces/index.js'
 import { GameComponent, TransformComponent } from '../components/index.js'
-import { ContainerComponentRepository } from '../repositories/index.js'
+import { ContainerComponentRepository, ContainerTagRepository } from '../repositories/index.js'
 
-export class GameObject implements IGameObject {
+export class GameObject implements IGameObject, IContainerComponent, ITaggable {
 
   private _isDestroyed: boolean
 
   private _containerComponent: ContainerComponentRepository
+  private _containerTag: ContainerTagRepository
   private _transform: TransformComponent
 
   get transform() { return this._transform }
@@ -20,6 +21,7 @@ export class GameObject implements IGameObject {
   start() {
     this._isDestroyed = false
 
+    this._containerTag = new ContainerTagRepository()
     this._containerComponent = new ContainerComponentRepository()
     this._transform = new TransformComponent()
 
@@ -57,5 +59,21 @@ export class GameObject implements IGameObject {
 
   getComponents() {
     return this._containerComponent.getComponents()
+  }
+
+  addTag(tag: Tag): void {
+    this._containerTag.add(tag)
+  }
+
+  removeTag(tag: Tag): void {
+    this._containerTag.remove(tag)
+  }
+
+  hasTag(...tags: Tag[]): void {
+    this._containerTag.has(...tags)
+  }
+
+  getTags() {
+    return Array.from<Tag>(this._containerTag.getTags())
   }
 }
