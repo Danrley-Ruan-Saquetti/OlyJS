ca2314adcc6a9b20fdc1b72a4f0b94d73491e542
Dan Ruan <danrsaquetti@gmail.com>
2025-02-07 23:06:45 -0300

    feat: Creating player animation and adding motion control

--- src/tests/animation/entities/player.entity.ts
+++ src/tests/animation/entities/player.entity.ts
@@ -1,39 +1,29 @@
 import { ImageSprite } from '../../../components/index.js'
 import { GameObject } from '../../../entities/index.js'
-import { CanvasRenderer, Timeout } from '../../../utils/index.js'
+import { Keys } from '../../../enums/key.enum.js'
+import { Animator, CanvasRenderer, DeltaTime, Input, StateAnimation } from '../../../utils/index.js'
+import {
+  PLAYER_STATE_WALK_DOWN_FRAMES,
+  PLAYER_STATE_WALK_LEFT_FRAMES,
+  PLAYER_STATE_WALK_RIGHT_FRAMES,
+  PLAYER_STATE_WALK_UP_FRAMES,
+  PLAYER_STATE_IDLE_LEFT_FRAMES,
+  PLAYER_STATE_IDLE_RIGHT_FRAMES,
+  PLAYER_STATE_IDLE_UP_FRAMES,
+  PLAYER_STATE_IDLE_DOWN_FRAMES,
+} from '../animation/states/player.state.js'
 
 export class Player extends GameObject {
 
   body: ImageSprite
 
-  private frames = [
-    {
-      origin: { x: 6, y: 554 },
-      dimension: { width: 31, height: 90 },
-    },
-    {
-      origin: { x: 73, y: 551 },
-      dimension: { width: 31, height: 87 },
-    },
-    {
-      origin: { x: 140, y: 551 },
-      dimension: { width: 31, height: 87 },
-    },
-    {
-      origin: { x: 203, y: 554 },
-      dimension: { width: 37, height: 90 },
-    },
-    {
-      origin: { x: 273, y: 554 },
-      dimension: { width: 31, height: 90 },
-    },
-    {
-      origin: { x: 340, y: 551 },
-      dimension: { width: 31, height: 87 },
-    },
-  ]
+  private animator: Animator
 
-  private frameIndex = -1
+  private speed = 100
+  private speedAnimation = 200
+  private speedAnimationBoosted = this.speedAnimation + 50
+  private direction = 'down'
+  private isBoostSpeed = false
 
   constructor(
     public spriteImage: HTMLImageElement
@@ -49,25 +39,173 @@ export class Player extends GameObject {
       this.spriteImage
     )
 
-    this.toggleFrame()
+    this.animator = new Animator()
 
-    Timeout.setInterval(() => this.toggleFrame(), 200)
+    this.animator.addAnimationState(new StateAnimation({
+      key: 'idle-down',
+      frames: PLAYER_STATE_IDLE_DOWN_FRAMES,
+      delay: this.speedAnimation,
+      sprite: this.body,
+      runOnStart: true,
+    }))
+    this.animator.addAnimationState(new StateAnimation({
+      key: 'idle-up',
+      frames: PLAYER_STATE_IDLE_UP_FRAMES,
+      delay: this.speedAnimation,
+      sprite: this.body,
+      runOnStart: true,
+    }))
+    this.animator.addAnimationState(new StateAnimation({
+      key: 'idle-left',
+      frames: PLAYER_STATE_IDLE_LEFT_FRAMES,
+      delay: this.speedAnimation,
+      sprite: this.body,
+      runOnStart: true,
+    }))
+    this.animator.addAnimationState(new StateAnimation({
+      key: 'idle-right',
+      frames: PLAYER_STATE_IDLE_RIGHT_FRAMES,
+      delay: this.speedAnimation,
+      sprite: this.body,
+      runOnStart: true,
+    }))
+    this.animator.addAnimationState(new StateAnimation({
+      key: 'walk-down',
+      frames: PLAYER_STATE_WALK_DOWN_FRAMES,
+      delay: this.speedAnimation,
+      sprite: this.body,
+      isRepeat: true,
+      runOnStart: true,
+    }))
+    this.animator.addAnimationState(new StateAnimation({
+      key: 'walk-up',
+      frames: PLAYER_STATE_WALK_UP_FRAMES,
+      delay: this.speedAnimation,
+      sprite: this.body,
+      isRepeat: true,
+      runOnStart: true,
+    }))
+    this.animator.addAnimationState(new StateAnimation({
+      key: 'walk-left',
+      frames: PLAYER_STATE_WALK_LEFT_FRAMES,
+      delay: this.speedAnimation,
+      sprite: this.body,
+      isRepeat: true,
+      runOnStart: true,
+    }))
+    this.animator.addAnimationState(new StateAnimation({
+      key: 'walk-right',
+      frames: PLAYER_STATE_WALK_RIGHT_FRAMES,
+      delay: this.speedAnimation,
+      sprite: this.body,
+      isRepeat: true,
+      runOnStart: true,
+    }))
+    this.animator.addAnimationState(new StateAnimation({
+      key: 'walk-down-speed',
+      frames: PLAYER_STATE_WALK_DOWN_FRAMES,
+      delay: this.speedAnimationBoosted,
+      sprite: this.body,
+      isRepeat: true,
+      runOnStart: true,
+    }))
+    this.animator.addAnimationState(new StateAnimation({
+      key: 'walk-up-speed',
+      frames: PLAYER_STATE_WALK_UP_FRAMES,
+      delay: this.speedAnimationBoosted,
+      sprite: this.body,
+      isRepeat: true,
+      runOnStart: true,
+    }))
+    this.animator.addAnimationState(new StateAnimation({
+      key: 'walk-left-speed',
+      frames: PLAYER_STATE_WALK_LEFT_FRAMES,
+      delay: this.speedAnimationBoosted,
+      sprite: this.body,
+      isRepeat: true,
+      runOnStart: true,
+    }))
+    this.animator.addAnimationState(new StateAnimation({
+      key: 'walk-right-speed',
+      frames: PLAYER_STATE_WALK_RIGHT_FRAMES,
+      delay: this.speedAnimationBoosted,
+      sprite: this.body,
+      isRepeat: true,
+      runOnStart: true,
+    }))
 
     this.addComponent(this.body)
   }
 
-  toggleFrame() {
-    this.frameIndex++
+  update(deltaTime: DeltaTime) {
+    let directionMoved: string | null = null
 
-    if (this.frameIndex == this.frames.length) {
-      this.frameIndex = 0
+    let boostSpeed = 0
+
+    if (Input.keyboard.isKeyDown(Keys.ShiftLeft)) {
+      if (!this.isBoostSpeed) {
+        this.animator.getCurrentState().delay = 100
+      }
+
+      this.isBoostSpeed = true
+      boostSpeed = 50
+    } else {
+      if (this.isBoostSpeed) {
+        this.animator.getCurrentState().delay = 200
+      }
+
+      this.isBoostSpeed = false
     }
 
-    this.body.origin.x = this.frames[this.frameIndex].origin.x
-    this.body.origin.y = this.frames[this.frameIndex].origin.y
+    if (Input.keyboard.isKeyDown(Keys.KeyW)) {
+      this.transform.translate({
+        y: -(this.speed + boostSpeed) * deltaTime.elapsedTimeSeconds
+      })
 
-    this.body.dimension.width = this.frames[this.frameIndex].dimension.width
-    this.body.dimension.height = this.frames[this.frameIndex].dimension.height
+      directionMoved = 'up'
+    } else if (Input.keyboard.isKeyDown(Keys.KeyS)) {
+      this.transform.translate({
+        y: (this.speed + boostSpeed) * deltaTime.elapsedTimeSeconds
+      })
+
+      directionMoved = 'down'
+    }
+    if (Input.keyboard.isKeyDown(Keys.KeyA)) {
+      this.transform.translate({
+        x: -(this.speed + boostSpeed) * deltaTime.elapsedTimeSeconds
+      })
+
+      directionMoved = 'left'
+    } else if (Input.keyboard.isKeyDown(Keys.KeyD)) {
+      this.transform.translate({
+        x: (this.speed + boostSpeed) * deltaTime.elapsedTimeSeconds
+      })
+
+      directionMoved = 'right'
+    }
+
+    if (directionMoved) {
+      this.walk(directionMoved)
+    } else {
+      this.idle()
+    }
+  }
+
+  walk(direction: string) {
+    let keyAnimation = `walk-${direction}`
+
+    if (this.isBoostSpeed) {
+      keyAnimation += '-speed'
+    }
+
+    if (this.animator.activeState(keyAnimation, { ignoreIfAlreadyActive: true })) {
+      this.isBoostSpeed = false
+    }
+    this.direction = direction
+  }
+
+  idle() {
+    this.animator.activeState(`idle-${this.direction}`, { ignoreIfAlreadyActive: true })
   }
 
   render(canvasRenderer: CanvasRenderer) {
