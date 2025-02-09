972980eee0271bdbc63741d8ab9b97f01bf605bc
Dan Ruan <danrsaquetti@gmail.com>
2025-02-08 21:57:45 -0300

    feat: Adding followTarget property to the camera game object to indicate the target that should follow

--- src/entities/camera.entity.ts
+++ src/entities/camera.entity.ts
@@ -1,8 +1,11 @@
+import { IVector2 } from '../interfaces/index.js'
+import { DeltaTime } from '../utils/delta-time.js'
 import { GameObject } from './game-object.entity.js'
 
 export class CameraGameObject extends GameObject {
 
   private _scale: number
+  private followTarget?: IVector2
 
   get scale() { return this._scale }
   set scale(value) { this._scale = value }
@@ -11,5 +14,13 @@ export class CameraGameObject extends GameObject {
     super.start()
 
     this._scale = 1
+  }
+
+  follow(target: IVector2) {
+    this.followTarget = target
+  }
+
+  getFollowTarget() {
+    return this.followTarget
   }
 }
