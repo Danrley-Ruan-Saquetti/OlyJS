3499679ae888b006baa83bdc45e912228de4b516
Dan Ruan <danrsaquetti@gmail.com>
2025-02-14 20:45:58 -0300

    feat: Changing the default circle shape radius size

--- src/components/shapes/circle.shape.ts
+++ src/components/shapes/circle.shape.ts
@@ -8,7 +8,7 @@ export class CircleShapeComponent extends ShapeComponent implements ICircle {
   set radius(value) { this._radius = value }
 
   constructor(
-    public _radius = 1
+    public _radius = 25
   ) {
     super()
   }
