5e3db0cbccea214725e27566f14cff3027801695
Dan Ruan <danrsaquetti@gmail.com>
2025-12-16 13:31:36 -0300

    feat: Criando componente de filas para efetuar para processamento via flush - `EventQueue`

---
+++ src/utils/event/event-queue.ts
@@ -0,0 +1,27 @@
+import { EventMap, EventName } from '@utils/event/types'
+
+type EventBuffer<Type extends EventName = any, Payload = any> = {
+  type: keyof Type
+  payload: Payload
+}
+
+export class EventQueue<Events extends EventMap = any> {
+
+  private buffer: EventBuffer[] = []
+  private swap: EventBuffer[] = []
+
+  push<KEvent extends keyof Events>(type: KEvent, payload: Events[KEvent]) {
+    this.buffer.push({ type, payload })
+  }
+
+  flush() {
+    const temp = this.buffer
+
+    this.buffer = this.swap
+    this.swap = temp
+
+    this.buffer.length = 0
+
+    return this.swap
+  }
+}
