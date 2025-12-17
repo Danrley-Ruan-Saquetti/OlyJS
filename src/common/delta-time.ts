f744c1f63fa92a1e62de2abe360cfa2252869491
Dan Ruan <danrsaquetti@gmail.com>
2025-12-17 16:22:56 -0300

    feat: Criando componente `DeltaTimer` para contagem do tempo decorrido entre ticks

---
+++ src/common/delta-time.ts
@@ -0,0 +1,37 @@
+export interface DeltaTime {
+  deltaTime: number
+  deltaTimeSeconds: number
+  totalElapsedTime: number
+}
+
+export class DeltaTimer {
+
+  protected lastTime = 0
+  protected totalElapsedTime = 0
+  protected deltaTime = 0
+
+  constructor() {
+    this.lastTime = performance.now()
+  }
+
+  reset() {
+    this.lastTime = performance.now()
+    this.totalElapsedTime = 0
+  }
+
+  tick() {
+    const currentTime = performance.now()
+
+    this.deltaTime = currentTime - this.lastTime
+    this.totalElapsedTime += this.deltaTime
+    this.lastTime = currentTime
+  }
+
+  getState(): DeltaTime {
+    return {
+      deltaTime: this.deltaTime,
+      deltaTimeSeconds: this.deltaTime / 1000,
+      totalElapsedTime: this.totalElapsedTime,
+    }
+  }
+}
