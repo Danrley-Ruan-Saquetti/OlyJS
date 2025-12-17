b1f6ff4d4df16c98623841b502f11b6262abb4cd
Dan Ruan <danrsaquetti@gmail.com>
2025-12-17 16:08:12 -0300

    feat: Criando componente "common" `EventQueue` para controle da fila de eventos

---
+++ src/common/event/event-queue.ts
@@ -0,0 +1,27 @@
+import { EventMap, EventName } from '@common/event/types'
+
+type EventBuffer<Event extends EventName = any, Data = any> = {
+  event: Event
+  data: Data
+}
+
+export class EventQueue<Events extends EventMap = any> {
+
+  private buffer: EventBuffer[]
+  private swap: EventBuffer[]
+
+  push<E extends keyof Events>(event: E, data: Events[E]) {
+    this.buffer.push({ event, data })
+  }
+
+  flush() {
+    const temp = this.buffer
+
+    this.buffer = this.swap
+    this.swap = temp
+
+    this.buffer.length = 0
+
+    return this.swap
+  }
+}
