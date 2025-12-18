ded9db6a7fb96116fa926183a8f3ed13ab0d336e
Dan Ruan <danrsaquetti@gmail.com>
2025-12-18 16:10:00 -0300

    test: Criando exemplo de utilização da Engine

--- tests/index.js
+++ tests/index.js
@@ -1,2 +1,18 @@
-import { } from '../build/dist/index.js';
+import { Engine } from '../dist/index.js';
 
+const engine = new Engine()
+
+engine.on('engine:start', () => {
+  console.log('Engine started')
+})
+
+engine.on('engine:test', (data) => {
+  console.log(data)
+})
+
+engine.start()
+engine.send('engine:test', 'Hello World')
+engine.send('engine:test', 'Hello World')
+engine.tick()
+engine.send('engine:test', 'Hello World')
+engine.tick()
