97f5bb41be7990b4aa8d9c97b83a60793ab9b48c
Dan Ruan <danrsaquetti@gmail.com>
2025-12-21 13:38:24 -0300

    feat: Adicionando método `initialize` e movendo lógica de update da Engine para o método `updateEngine`

--- src/game/game.ts
+++ src/game/game.ts
@@ -1,33 +1,52 @@
-import { IEngine } from '../engine'
-import { EventMap } from '../runtime'
+import { ISystem } from '../ecs/system'
+import { Engine } from '../engine/engine'
+import { IEngine } from '../engine/types'
+import { EventMap } from '../runtime/contracts/event'
 
 export class Game<Events extends EventMap = {}> {
 
   private timeout: number
   private lastTime = 0
 
-  constructor(
-    protected engine: IEngine<Events>
-  ) { }
+  protected engine: IEngine<Events> = new Engine<Events>()
 
   start() {
+    if (this.engine.isRunning()) {
+      return
+    }
+
+    this.initialize()
     this.engine.start()
 
     this.update(0)
   }
 
+  protected initialize() { }
+
   stop() {
+    if (!this.engine.isRunning()) {
+      return
+    }
+
     this.engine.stop()
 
     cancelAnimationFrame(this.timeout)
   }
 
   protected update = (timestamp: number) => {
+    this.updateEngine(timestamp)
+    this.timeout = requestAnimationFrame(this.update)
+  }
+
+  private updateEngine(timestamp: number) {
     const deltaTime = timestamp - this.lastTime
 
     this.engine.tick(deltaTime)
 
     this.lastTime = timestamp
-    this.timeout = requestAnimationFrame(this.update)
+  }
+
+  registerSystem(system: ISystem<Events>) {
+    this.engine.registerSystem(system)
   }
 }
