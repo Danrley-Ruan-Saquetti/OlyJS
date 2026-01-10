5d932860cb7b47db5631569083dad65e3042ec1c
Dan Ruan <danrsaquetti@gmail.com>
2026-01-10 19:46:18 -0300

    remove: Removendo pacote "game-runtime/component"

--- src/game-runtime/component/types.ts
+++
@@ -1,9 +0,0 @@
-import { EntityId } from '../../ecs/entity'
-
-export interface IComponentStorage<T = unknown> {
-  has(entity: EntityId): boolean
-  get(entity: EntityId): T | undefined
-  set(entity: EntityId, value: T): void
-  delete(entity: EntityId): void
-  entities(): Iterable<EntityId>
-}
