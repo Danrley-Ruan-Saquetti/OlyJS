fc57c523e56920a78fbf29ecf5db527785639ac8
Dan Ruan <danrsaquetti@gmail.com>
2026-01-21 20:55:33 -0300

    remove: Removendo propriedade `ids` não utilizada

--- src/runtime/world/query.ts
+++ src/runtime/world/query.ts
@@ -1,21 +1,18 @@
 import { IArchetype, Signature } from '../../ecs/archetype'
-import { ComponentId } from '../../ecs/component'
 import { ComponentDescriptor } from './component'
 
 export class Query {
 
-  private ids: ComponentId[] = []
   private mask: Signature = 0n
 
   private readonly matched: IArchetype[] = []
 
   constructor(
-    private archetypes: Map<string, IArchetype>,
+    private readonly archetypes: Map<string, IArchetype>,
     components: ComponentDescriptor[] = []
   ) {
     let i = 0, length = components.length
     while (i < length) {
-      this.ids.push(components[i].id)
       this.mask |= 1n << components[i].id
       i++
     }
@@ -41,7 +38,7 @@ export class Query {
     }
   }
 
-  getArchetypes(): readonly IArchetype[] {
+  view(): readonly IArchetype[] {
     return this.archetypes as any
   }
 }
