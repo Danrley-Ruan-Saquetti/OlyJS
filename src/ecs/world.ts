af167e59c0c91c8e400baa8059472885a10ab4b6
Dan Ruan <danrsaquetti@gmail.com>
2026-01-21 08:12:59 -0300

    feat: Adicionando métodos de operação na interface `IWorld`

--- src/ecs/world.ts
+++ src/ecs/world.ts
@@ -1,3 +1,12 @@
-export interface IWorld {
+import { ComponentDescriptor } from '../runtime/world/component'
+import { Query } from '../runtime/world/query'
+import { EntityId } from './entity'
 
+export interface IWorld {
+  instantiate(): EntityId
+  destroy(entityId: EntityId): void
+  addComponent(entityId: EntityId, component: ComponentDescriptor): void
+  registerComponent(descriptor: ComponentDescriptor): void
+  flush(): void
+  createQuery(components: ComponentDescriptor[]): Query
 }
