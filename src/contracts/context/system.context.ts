afd3a32b6015a0dfafaf23bc16459c9c1bb77f5d
Dan Ruan <danrsaquetti@gmail.com>
2026-02-01 10:09:42 -0300

    remove: Removendo propriedades "world", "events" e "commands" da interfacer `SystemContext`

--- src/contracts/context/system.context.ts
+++ src/contracts/context/system.context.ts
@@ -1,6 +1,6 @@
 import { IWorld } from '../../ecs/world'
 import { ICommandDomainRegister } from '../engine/command'
-import { IEventPublisher, IEventSink } from '../engine/event'
+import { IEventPublisher } from '../engine/event'
 import { Input } from '../engine/input'
 import { DeltaTime } from '../engine/time'
 
@@ -11,9 +11,6 @@ export interface SystemInitializeContext {
 }
 
 export interface SystemContext {
-  readonly world: IWorld
   readonly time: DeltaTime
   readonly input: Input
-  readonly events: IEventSink
-  readonly commands: ICommandDomainRegister
 }
