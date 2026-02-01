3f8da80291de14a637774a287b3e89c27e1353e2
Dan Ruan <danrsaquetti@gmail.com>
2026-02-01 16:14:48 -0300

    remove: Removendo propriedades da classe `MutableSystemUpdateContext` não definidas na interface `SystemUpdateContext`

--- src/game-runtime/mutable-system-update-context.ts
+++ src/game-runtime/mutable-system-update-context.ts
@@ -1,14 +1,8 @@
 import { SystemUpdateContext } from '../contracts/context/system.context'
-import { ICommandDomainRegister } from '../contracts/engine/command'
-import { IEventSink } from '../contracts/engine/event'
 import { Input } from '../contracts/engine/input'
 import { DeltaTime } from '../contracts/engine/time'
-import { IWorld } from '../ecs/world'
 
 export class MutableSystemUpdateContext implements SystemUpdateContext {
-  world: IWorld
   time: DeltaTime
   input: Input
-  events: IEventSink
-  commands: ICommandDomainRegister
 }
