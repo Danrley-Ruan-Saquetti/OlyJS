2487d74e490cea1d889a1edc8fc2b245423f2360
Dan Ruan <danrsaquetti@gmail.com>
2026-02-08 15:38:14 -0300

    feat: Implementando todos os métodos de consulta definidos na interface `IQuery`

--- src/runtime/world/query/query.ts
+++ src/runtime/world/query/query.ts
@@ -1,15 +1,16 @@
 import { IArchetype, Signature } from '../../../ecs/archetype'
 import { ComponentIdentifier } from '../../../ecs/component'
+import { EntityId } from '../../../ecs/entity'
 import { createSignature } from '../archetype/create-signature'
+import { IQuery } from './../../../ecs/query'
 
-export class Query {
+export class Query implements IQuery {
 
-  private readonly mask: Signature = 0n
+  readonly mask: Signature = 0n
 
   private readonly matched: IArchetype[] = []
 
   constructor(
-    private readonly archetypes: Map<string, IArchetype>,
     components: ComponentIdentifier[] = []
   ) {
     this.mask = createSignature(components)
@@ -23,10 +24,10 @@ export class Query {
     this.matched.push(archetype)
   }
 
-  build() {
+  build(archetypes: MapIterator<IArchetype>) {
     this.matched.length = 0
 
-    for (const archetype of this.archetypes.values()) {
+    for (const archetype of archetypes) {
       if ((archetype.signature & this.mask) !== this.mask) {
         continue
       }
@@ -37,5 +38,95 @@ export class Query {
 
   view() {
     return this.matched as readonly IArchetype[]
+  }
+
+  find(max?: number) {
+    if (max) {
+      return this.findLimited(max)
+    }
+
+    return this.findAll()
+  }
+
+  findFirst() {
+    let i = 0, length = this.matched.length
+    while (i < length) {
+      if (this.matched[i].size) {
+        return this.matched[i].entities[0]
+      }
+
+      i++
+    }
+
+    return undefined
+  }
+
+  count() {
+    let count = 0
+
+    let i = 0, length = this.matched.length
+    while (i < length) {
+      count += this.matched[i].size
+      i++
+    }
+
+    return count
+  }
+
+  isEmpty() {
+    let i = 0, length = this.matched.length
+    while (i < length) {
+      if (this.matched[i].size) {
+        return false
+      }
+
+      i++
+    }
+
+    return true
+  }
+
+  private findAll() {
+    const entitiesId: EntityId[] = []
+
+    let i = 0, length = this.matched.length
+    while (i < length) {
+      const entities = this.matched[i].entities
+
+      let j = 0, size = this.matched[i].size
+      while (j < size) {
+        entitiesId.push(entities[j])
+
+        j++
+      }
+
+      i++
+    }
+
+    return entitiesId
+  }
+
+  private findLimited(max: number) {
+    const entitiesId: EntityId[] = []
+
+    let i = 0, length = this.matched.length
+    while (i < length) {
+      const entities = this.matched[i].entities
+
+      let j = 0, size = this.matched[i].size
+      while (j < size) {
+        entitiesId.push(entities[j])
+
+        if (entitiesId.length >= max) {
+          return entitiesId
+        }
+
+        j++
+      }
+
+      i++
+    }
+
+    return entitiesId
   }
 }
